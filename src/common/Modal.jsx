0ac4600f3e820b2ebd18022af17b7cdfbc5afeba
Mateo2131 <goz.matu@gmail.com>
2022-08-06 19:43:14 -0300

    Lock body when a modal is open

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,11 +1,12 @@
 import { MdOutlineClose } from 'react-icons/md'
 
-function Modal ({ children, toggle, isOpen }) {
+function Modal ({ children, toggle, isOpen, ...props }) {
   return (
     <div
       className={`${
         isOpen ? 'right-0' : 'right-[-100%]'
       } h-screen fixed top-0 w-[250px] md:w-[300px] bg-white shadow-lg duration-500 ease-in-out`}
+      {...props}
     >
       {isOpen && (
         <div className='p-2 h-full'>
--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function useModal () {
   const [isOpen, setIsOpen] = useState(false)
 
   const toggle = () => setIsOpen(!isOpen)
+
+  useEffect(() => {
+    document.body.style.overflow = (isOpen) ? 'hidden' : 'auto'
+  }, [isOpen])
 
   return { isOpen, toggle }
 }
